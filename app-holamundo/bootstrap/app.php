b3cdd4f8238264a910f142a53baec2ce8215854e
Eliaser <eperyaq205@g.educaand.com>
2025-02-05 15:49:55 +0100

    cambios

---
+++ app-holamundo/bootstrap/app.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+        then: function () {
+            Route::prefix('usuarios')->group(base_path('routes/routes_usuarios.php'));
+            Route::prefix('productos')->group(base_path('routes/routes_producto.php'));
+        
+        }
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
---
+++ app-holamundo/resources/views/altausuario.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alta usuarios</title>
+</head>
+<body>
+    <h1>Alta usuarios</h1>
+</body>
+</html>
